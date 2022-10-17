6683b24b545663376a6b25159934524f49ff1a38
casteser <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-10-17 12:08:02 +0100

    .

--- script.js
+++ script.js
@@ -67,3 +67,17 @@ function moving(e) {
         dragobj.style.top = oTop + 'px';
     }
 }
+
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+        /*
+        inputSlider.oninput = (() => {
+            let value = inputSlider.value;
+            slideValue.textContent = value;
+            slideValue.style.left = (value / 2) + "%";
+            slideValue.classList.add("show");
+        });
+        inputSlider.onblur = (() => {
+            slideValue.classList.remove("show");
+        });
+        */
